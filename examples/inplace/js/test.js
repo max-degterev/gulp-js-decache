551320c62a989bc038f55bb20983c3cb19d7f87d
Max Degterev <max@degterev.me>
2019-11-29 15:51:45 +0100

    will this just work?

---
+++ examples/inplace/js/test.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+
+const simpleRef = '/plainDoge-700x525.jpg';
+
+const refWithQuery = '/plainDoge-700x525.jpg?with=querystring';
+
+const refWithHash = "plainDoge-700x525.jpg#ololohash";
+
+const refWithHashQuery = "plainDoge-700x525.jpg?tough=cookie#ololohash";
+
+const missingRef = "doesntexist.jpg";
+
+const decachedRef = '/plainDoge-700x525.jpg?decache=done';
+
+funcCall('/plainDoge-700x525.jpg');
+
+const myFunc = () => '/plainDoge-700x525.jpg';
---
+++ examples/inplace/public/test.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+
+const simpleRef = '/plainDoge-700x525.jpg?decache=43be11133aa8b5c875b86ea64b5cfa34';
+
+const refWithQuery = '/plainDoge-700x525.jpg?with=querystring&decache=43be11133aa8b5c875b86ea64b5cfa34';
+
+const refWithHash = "plainDoge-700x525.jpg?decache=43be11133aa8b5c875b86ea64b5cfa34#ololohash";
+
+const refWithHashQuery = "plainDoge-700x525.jpg?tough=cookie&decache=43be11133aa8b5c875b86ea64b5cfa34#ololohash";
+
+const missingRef = "doesntexist.jpg";
+
+const decachedRef = '/plainDoge-700x525.jpg?decache=done';
+
+funcCall('/plainDoge-700x525.jpg?decache=43be11133aa8b5c875b86ea64b5cfa34');
+
+const myFunc = () => '/plainDoge-700x525.jpg?decache=43be11133aa8b5c875b86ea64b5cfa34';
